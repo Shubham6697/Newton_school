29cd8ec745bce65c200bca7be9c8b079c23bacce
Shubham6697 <50530172+Shubham6697@users.noreply.github.com>
2021-06-07 18:41:47 +0530

    Update strictly_increasing_arr.java
    Let's see maybe it will work bro...

--- assignment/array/strictly_increasing_arr.java
+++ assignment/array/strictly_increasing_arr.java
@@ -11,59 +11,35 @@ public class Main {
         int len = sc.nextInt();
         int arr[] = new int[len];
         for (int i=0;i<len;i++)
-        {
             arr[i] = sc.nextInt();
-        }
+        
         // consider that first element should be replaced by one 
         arr[0] = 1;
-        boolean flag=false;
-        int start=2;
+        
         for (int i=1;i<len;i++)
         {
             if (i<len-1)
             {
-                if ((arr[i] > arr[i-1]) && (arr[i] < arr[i+1]))
-                {
-                    continue;
-                }
-                else
-                {
-                    for (int k=start;k<=arr[i]/2;k++)
+                    for (int k=a[i-1] + 1;k<=arr[i]/2 + 1;k++)
                     {
-                        if ((arr[i]%k==0) && ((k>arr[i-1]) && k<arr[i+1]))
+                        if (arr[i] % k == 0)
                         {
                                 arr[i] = k;
-                                start=k+1;
-                                flag=false;
                                 break;
                         }
-                        else
-                        {
-                            flag=true;
-                            break;
-                        }
                     }
-                }
             }
-            else
-            {
-                if ((arr[i] >= arr[i-1]))
+            
+     
+                if ((arr[i] <= arr[i-1]))
                 {
-                    break;
+                    System.out.println("No");
+                    System.exit(0);
                 }
-                else 
-                {
-                    flag=true;
-                }
-            }
+            
+     
         }
-        if (flag==true)
-        {
-            System.out.println("No");
-        }
-        else if (flag==false)
-        {
+          
             System.out.println("Yes");
-        }
     }
 }
